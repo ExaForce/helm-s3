0070f4ae5457cf7a36d671f46ab3aabc61eed87c
Vaclav Dvorak <vdvorak@exaforce.com>
2025-04-03 11:13:42 +0200

    fmt

--- internal/awss3/storage.go
+++ internal/awss3/storage.go
@@ -169,23 +169,23 @@ func processS3Object(ctx context.Context, client *s3.S3, bucket string, obj *s3.
 
 	maxRetries := 3
 	for attempt := 1; attempt <= maxRetries; attempt++ {
-			metaOut, err = client.HeadObjectWithContext(ctx, &s3.HeadObjectInput{
-					Bucket: aws.String(bucket),
-					Key:    obj.Key,
-			})
+		metaOut, err = client.HeadObjectWithContext(ctx, &s3.HeadObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    obj.Key,
+		})
 
-			if err == nil {
-					// Success, break out of the retry loop
-					break
-			}
+		if err == nil {
+			// Success, break out of the retry loop
+			break
+		}
 
-			if attempt < maxRetries {
-					log.Warnf("Attempt %d to head s3 object %q failed: %s. Retrying...", attempt, key, err)
-			} else {
-					// Final attempt failed
-					log.Errorf("head s3 object %q failed after %d attempts: %s", key, maxRetries, err)
-					return
-			}
+		if attempt < maxRetries {
+			log.Warnf("Attempt %d to head s3 object %q failed: %s. Retrying...", attempt, key, err)
+		} else {
+			// Final attempt failed
+			log.Errorf("head s3 object %q failed after %d attempts: %s", key, maxRetries, err)
+			return
+		}
 	}
 
 	reindexItem := ChartInfo{Filename: key}
